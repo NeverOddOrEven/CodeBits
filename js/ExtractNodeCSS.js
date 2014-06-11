5390272b08b241913bad10d1fe02dcb658ccdf6a
Alex Suttmiller <Alex@Alexs-MacBook-Pro.local>
2014-06-11 17:11:28 -0400

    add license

--- js/ExtractNodeCSS.js
+++ js/ExtractNodeCSS.js
@@ -1,4 +1,32 @@
-//$(document).ready(function()  {
+/* 
+* You can copy this code into your Chrome console code. 
+* You can then pretty print the css by passing the XPath of the node youre interested in
+* To get the XPath, you can use the inspector to select the element. Right click the element
+* in the inspector and Copy XPath.
+
+The MIT License (MIT)
+
+Copyright (c) 2014 Alex Suttmiller, Omniaery
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+
+*/
 CSSLIB = (function () {
   function allCss(element, doc) {
       var parentsCss = parentElementsCss(element, doc);
